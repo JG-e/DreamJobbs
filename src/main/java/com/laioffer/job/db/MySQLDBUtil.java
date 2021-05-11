17e450206f5c4e003c4f3901468205fd1ca1813d
JG-e <zezhengh@163.com>
2021-05-11 14:25:04 +0800

    replace password

--- src/main/java/com/laioffer/job/db/MySQLDBUtil.java
+++ src/main/java/com/laioffer/job/db/MySQLDBUtil.java
@@ -5,7 +5,7 @@ public class MySQLDBUtil {
     private static final String PORT_NUM = "3306";
     public static final String DB_NAME = "Jupiter";
     private static final String USERNAME = "admin";
-    private static final String PASSWORD = "Woshigzz2000";
+    private static final String PASSWORD = "REPLACE WITH YOURS";
     public static final String URL = "jdbc:mysql://"
             + INSTANCE + ":" + PORT_NUM + "/" + DB_NAME
             + "?user=" + USERNAME + "&password=" + PASSWORD
